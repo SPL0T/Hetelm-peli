be52fc8c6031afabff639b8c64b3887681b9c0cc
unknown <mahonen.samu@gmail.com>
2020-11-18 10:47:37 +0200

    Version 1.0

--- game.js
+++ game.js
@@ -6,6 +6,8 @@ const nameblock = document.querySelector("#name");
 const saldonaytto = document.querySelector("#saldo");
 const panos = document.querySelector("#panos");
 const vpanos = document.querySelector("#vpanos");
+const vvoitto = document.querySelector("#vvoitto");
+const mvoitto = document.querySelector("#mvoitto");
 
 let fruits = [f1, f2, f3, f4];
 let playerName= "";
@@ -17,13 +19,20 @@ let bet = 0;
 let lbet = 0;
 let x;
 let turn = 1;
+let lwin = 0;
+let mwin = 0;
 screenupdate();
-imgupdate2();
-/* Screen Update Function */
+
+/*
+Screen Update Functions
+*/
 function screenupdate() {
     panos.innerText = bet;
     saldonaytto.innerText = saldo;
     vpanos.innerText = lbet;
+    vvoitto.innerText = lwin
+    mvoitto.innerText = mwin
+    imgupdate2();
 }
 
 function imgupdate2(){
@@ -41,63 +50,49 @@ function imgupdate2(){
                 fruits[x].src = "/kuvat/vesimelooni.png";}}
 }}
 
-/* Locking number */
-    function locking() {
-        lockvalue[x] = numero[x];
-        console.log(lockvalue[x]);
-        console.log(lockvalue);
-    }
-
+/* 
+Name
+*/
 function nimi() {
     pelaaja = prompt("Anna nimesi");
     nameblock.innerText += pelaaja;
-    console.log(pelaaja);
-}
-
-function spin() {
-        numero = Array.from({length: 4}, () => Math.floor(Math.random() * 4 + 1));
-        console.log(numero);
-        screenupdate();
-        imgupdate2();
-}
-
-function poggers() {
-    if(lockvalue[0] && lockvalue[1] && lockvalue[2] && lockvalue[3] === 1) {
-        console.log("1111! Poggers")}
-    else if(lockvalue[0] && lockvalue[1] && lockvalue[2] && lockvalue[3] === 2) {
-        console.log("2222! Poggers")}
-    else if(lockvalue[0] && lockvalue[1] && lockvalue[2] && lockvalue[3] === 3) {
-        console.log("3333! Poggers")}
-    else if(lockvalue[0] && lockvalue[1] && lockvalue[2] && lockvalue[3] === 4) {
-        console.log("4444! Poggers")}
-    else {}
 }
 
 /*
 Betting functions
 */
 function e1() {
-    if(saldo > 0) {
+    if(saldo > 0 && turn == 1) {
     saldo -= 1
     bet += 1;
     screenupdate();}
-    else {alert("Sinulla ei ole tarpeeksi rahaa");}
-}
+    else if(turn == 1){
+        alert("Sinulla ei ole tarpeeksi rahaa");}
+    else{
+        alert("et voi laittaa rahaa muuta kuin pelin alussa")
+}}
 
 function e2() {
-    if(saldo > 1 ) {
+    if(saldo > 1 && turn == 1) {
     saldo -= 2;
     bet += 2;
     screenupdate();}
-    else {alert("Sinulla ei ole tarpeeksi rahaa");}
-}
+    else if(turn == 1){
+        alert("Sinulla ei ole tarpeeksi rahaa");}
+    else{
+        alert("et voi laittaa rahaa muuta kuin pelin alussa")
+}}
 
 function reset() {
+    if(turn == 1) {
     saldo += bet
     bet = 0
-    screenupdate()
-}
+    screenupdate()}
+    else{
+        alert("et voi ottaa panostasi kesken pelin")
+    }}
 
+/* Checks if you have money */
 function moneycheck() {
     if(turn == 1 && bet > 0) {
         game();
@@ -107,56 +102,76 @@ function moneycheck() {
     }
     else {
         game();
-        console.log("else");
     }
 }
 
+/*
+Locking number
+*/
+    function locking() {
+        lockvalue[x] = numero[x];
+    }
+
+/* Spin the rollers */
+function spin() {
+        numero = Array.from({length: 4}, () => Math.floor(Math.random() * 4 + 1));
+        screenupdate();
+}
+
+/* puts roller numbers to locking array if that spot isn't locked so you can see final numbers */
 function fcheck() {
     for(x=0; x < 4; x++) {
         if(lockvalue[x] == 0){
             lockvalue[x] = numero[x];
-            console.log(lockvalue)
-        }
-        else {
-            console.log("e toiminu")
-        }
-    }
-}
+        }}}
 
+/* Clears locking array for next turn */
 function clrlock() {
     for(x=0; x < 4; x++) {
-        if(lockvalue[x] != 0){
-            lockvalue[x] = 0;
-            console.log(lockvalue)
-        }
-        else {
-            console.log("e toiminu2")
-        }
+        (lockvalue[x] != 0)
+        lockvalue[x] = 0;      
     }
 }
 
+/* Functions so tha game knows what's going on */
 function game() {
     if(turn == 1) {
-        lbet = bet;
-        bet = 0;
+        clrlock();
         turn = 2;
-        console.log(turn)
         spin();
-        screenupdate;
-        console.log("paska")
+        screenupdate();
     }
     else if(turn == 2) {
-        bet = 0;
-        console.log(turn)
         spin();
-        screenupdate;
-        console.log("testi")
+        screenupdate();
         turn = 1;
-        console.log(turn)
         fcheck();
-        poggers();
-        clrlock();
+        wcheck();
+        lbet = bet;
+        bet = 0;
+        maxwin();
+        screenupdate();
+    }}
+
+/* Checks if you've won something */
+function wcheck() {
+    if(lockvalue[0] === 1 && lockvalue[1] === 1 && lockvalue[2] === 1 && lockvalue[3] === 1) {
+        saldo += bet * 2;
+        lwin = bet * 2;}
+    else if(lockvalue[0] === 2 && lockvalue[1] === 2 && lockvalue[2] === 2 && lockvalue[3] === 2) {
+        saldo += bet * 4;
+        lwin = bet * 4;}
+    else if(lockvalue[0] === 3 && lockvalue[1]  === 3&& lockvalue[2] === 3 && lockvalue[3] === 3) {
+        saldo += bet * 6;
+        lwin = bet * 6;}
+    else if(lockvalue[0] === 4 && lockvalue[1] === 4 && lockvalue[2] === 4 && lockvalue[3] === 4) {
+        saldo += bet * 8;
+        lwin = bet * 8;}
     }
-    else {
-    console.log("vittu")
-    screenupdate;}}
+
+/* puts max win on the infoboard */
+function maxwin(){
+    if(lwin > mwin){
+        mwin = lwin
+    }
+}
